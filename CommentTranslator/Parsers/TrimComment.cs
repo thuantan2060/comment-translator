ff8f5447137f9ff5f905ff415192aa36f062d39c
thuantan2060 <thuantan2060@gmail.com>
2017-10-04 14:35:43 +0700

    #Add translate comment pharagrap

---
+++ CommentTranslator/Parsers/TrimComment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommentTranslator.Parsers
+{
+    public class TrimComment
+    {
+        public string OriginText { get; set; }
+        public string TrimedText { get; set; }
+        public int LineCount { get; set; }
+    }
+}
